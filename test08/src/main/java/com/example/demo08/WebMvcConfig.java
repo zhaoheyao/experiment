01ca044f75da4caf12e403f731f215e1d240b455
zhaoshupeng <464254690@qq.com>
2019-05-19 20:35:34 +0800

    提交于2019/5/19

---
+++ test08/src/main/java/com/example/demo08/WebMvcConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo08;
+
+import com.example.demo08.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        registry.addInterceptor(loginInterceptor)
+
+                .addPathPatterns("/api/**")
+
+                .excludePathPatterns("/api/login")
+
+                .excludePathPatterns("/api/register");
+
+    }
+
+}
